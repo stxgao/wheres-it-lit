1e432860d6b50d0fa299e52cfb5cc83e545e40a5
stevenxgao <sgao1996@gmail.com>
2016-02-27 18:39:21 -0800

    Update map.js
    now draws geodesic lines between dropped markers.

--- map.js
+++ map.js
@@ -1,25 +1,75 @@
+var venues = [
+{lat: 49.2611, lng: -123.2531},
+{lat: 49.2611, lng: 123.5234},
+];
+
+var markers = [];
+
+var map;
+var flightPath;
+
 function initalize() {
 	var myOptions = {
-		zoom: 8,
+		zoom: 10,
 		center: new google.maps.LatLng(49.2611, -123.2531),
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	map = new google.maps.Map(document.getElementById("map"), myOptions);
 
-// This event listener calls addMarker() when the map is clicked.
-  google.maps.event.addListener(map, 'click', function(event) {
-    addMarker(event.latLng, map);
-  });
+	flightPath = new google.maps.Polyline({
+		path: [],
+		strokeColor: '#000000',
+		strokeOpacity: 1.0,
+		strokeWeight: 3,
+		geodesic: true
+	});
+	flightPath.setMap(map);
+}
+
+function drop() {
+	clearMarkers();
+	flightPath.setMap(map);
+	for (var i = 0; i < venues.length; i++) {
+		addMarkerWithTimeout(venues[i], i * 300);
+	}
+
+	for (var i = 0; i < venues.length; i++) {
+		addLatLng(venues[i], i);
+	}
 
 }
 
-// Adds a marker to the map.
-function addMarker(location, map) {
+// Handles click events on a map, and adds a new point to the Polyline.
+function addLatLng(location, i) {
+	var path = flightPath.getPath();
 
-  var marker = new google.maps.Marker({
-    position: location,
-    map: map
-  });
+  // Because path is an MVCArray, we can simply append a new coordinate
+  // and it will automatically appear.
+  window.setTimeout(function() {
+
+  	path.push(new google.maps.LatLng(location.lat, location.lng));
+  },i * 600);
+
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+function addMarkerWithTimeout(location, timeout) {
+	window.setTimeout(function() {
+		markers.push(new google.maps.Marker({
+			position: location,
+			map: map,
+			animation: google.maps.Animation.DROP
+		}));
+	}, timeout);
+
+
+}
+
+function clearMarkers() {
+	for (var i = 0; i < markers.length; i++) {
+		markers[i].setMap(null);
+	}
+	markers = [];
+	flightPath.setMap(null);
+	flightPath.getPath().clear();
+
+}
