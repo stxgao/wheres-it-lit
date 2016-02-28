a68f3700c405c6939397e2498e0aa9a975b53235
Matthew Siu <siunami.matt@gmail.com>
2016-02-28 01:12:04 -0800

    Delete search.js

--- search.js
+++
@@ -1,8 +0,0 @@
-function setURL(){
-	var url1 = "bandsintown.com/";
-	var urlName = input1.value.replace(/ /g,"%20");
-	//alert(urlName);
-	var urlEnd = "/events?format=json&app_id=YOUR_APP_ID&date=all";
-	var comb = url1 + urlName + urlEnd;
-	alert(comb);
-}
