6a266d3582648fdd02e9bfb671fbfa162bb1f6c2
mike4087 <mike4087@gmail.com>
2016-02-27 17:23:37 -0800

    Added files via upload

--- jscript.js
+++ jscript.js
@@ -1,6 +1,7 @@
 function setURL(){
 	var url1 = "bandsintown.com/";
-	var url2 = input1.value;
-	var comb = url1.concat(url2);
+	var urlFirst = input1.value;
+	var urlLast = input2.value;
+	var comb = url1 + urlFirst + "%20" + urlLast;
 	alert(comb);
 }
